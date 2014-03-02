db352d0a06b964cddacb483b73ba279bd2be22dc
Anton Junaidi <madara_clan@live.com>
2014-03-02 19:29:03 +0700

    updated

---
+++ coding/application/logs/log-2014-03-02.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2014-03-02 18:52:25 --> Severity: Notice  --> Trying to get property of non-object /Applications/XAMPP/xamppfiles/htdocs/activorm/coding/application/views/n/project/project_detail_view.php 183
+ERROR - 2014-03-02 18:52:25 --> Severity: Notice  --> Trying to get property of non-object /Applications/XAMPP/xamppfiles/htdocs/activorm/coding/application/views/n/project/project_detail_view.php 186
+ERROR - 2014-03-02 18:52:25 --> Severity: Notice  --> Trying to get property of non-object /Applications/XAMPP/xamppfiles/htdocs/activorm/coding/application/views/n/project/project_detail_view.php 186
+ERROR - 2014-03-02 18:52:25 --> Severity: Notice  --> Trying to get property of non-object /Applications/XAMPP/xamppfiles/htdocs/activorm/coding/application/views/n/project/project_detail_view.php 197
+ERROR - 2014-03-02 18:52:25 --> Severity: Notice  --> Trying to get property of non-object /Applications/XAMPP/xamppfiles/htdocs/activorm/coding/application/views/n/project/project_detail_view.php 183
+ERROR - 2014-03-02 18:52:25 --> Severity: Notice  --> Trying to get property of non-object /Applications/XAMPP/xamppfiles/htdocs/activorm/coding/application/views/n/project/project_detail_view.php 186
+ERROR - 2014-03-02 18:52:25 --> Severity: Notice  --> Trying to get property of non-object /Applications/XAMPP/xamppfiles/htdocs/activorm/coding/application/views/n/project/project_detail_view.php 186
+ERROR - 2014-03-02 18:52:25 --> Severity: Notice  --> Trying to get property of non-object /Applications/XAMPP/xamppfiles/htdocs/activorm/coding/application/views/n/project/project_detail_view.php 197
+ERROR - 2014-03-02 18:52:25 --> Severity: Notice  --> Trying to get property of non-object /Applications/XAMPP/xamppfiles/htdocs/activorm/coding/application/views/n/project/project_detail_view.php 183
+ERROR - 2014-03-02 18:52:25 --> Severity: Notice  --> Trying to get property of non-object /Applications/XAMPP/xamppfiles/htdocs/activorm/coding/application/views/n/project/project_detail_view.php 186
+ERROR - 2014-03-02 18:52:25 --> Severity: Notice  --> Trying to get property of non-object /Applications/XAMPP/xamppfiles/htdocs/activorm/coding/application/views/n/project/project_detail_view.php 186
+ERROR - 2014-03-02 18:52:25 --> Severity: Notice  --> Trying to get property of non-object /Applications/XAMPP/xamppfiles/htdocs/activorm/coding/application/views/n/project/project_detail_view.php 197
